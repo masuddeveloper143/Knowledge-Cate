57c97469d8f34ed5f950ba6b78638edf0a442d84
Masud Rana <md.masudrana.developer@gmail.com>
2026-04-07 23:58:35 +0800

    Update project

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,20 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
-      <Blogs></Blogs>
-      
+
+
+      <div className='min-container flex text-center'>
+        <div className='left-container w-[70%]'>
+
+
+
+          <Blogs></Blogs>
+        </div>
+        <div className='right-container w-[30%]'>
+          <h1>Reading Time: 0</h1>
+          <h1> bookmarked count: 0</h1>
+        </div>
+      </div>
 
     </>
   )
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import Blogg from '../Blogg/Blogg';
 
 const blogs = () => {
-const [blogs, setBlogs] = useState([])
+    const [blogs, setBlogs] = useState([])
 
 
     useEffect(() => {
@@ -13,7 +14,13 @@ const [blogs, setBlogs] = useState([])
     console.log(blogs)
     return (
         <div>
-
+            <h1 className='text-3xl font-bold'>Total: {blogs.length}</h1>
+            <div className='all-blogs grid grid-cols-2'>
+                {
+                    blogs.map((blog) => (
+                        <Blogg key={blog.id} blog={blog}></Blogg>))
+                }
+            </div>
         </div>
     );
 };
