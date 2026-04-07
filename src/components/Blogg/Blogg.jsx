8f259018b09f0d6ebc012d76131a85db93b53e27
Masud Rana <md.masudrana.developer@gmail.com>
2026-04-08 00:12:41 +0800

    Update project

--- src/components/Blogg/Blogg.jsx
+++ src/components/Blogg/Blogg.jsx
@@ -9,8 +9,7 @@ const Blogg = ({ blog }) => {
             <div className="card bg-base-100 w-96 shadow-sm">
                 <figure>
                     <img
-                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                        alt="Shoes" />
+                        src={image}/>
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">Card Title</h2>
