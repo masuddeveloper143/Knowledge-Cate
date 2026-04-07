691d0d82adbdeb90f864bec2c51ef9f3d7716e01
Masud Rana <md.masudrana.developer@gmail.com>
2026-04-08 00:44:56 +0800

    Update project

--- src/components/Blogg/Blogg.jsx
+++ src/components/Blogg/Blogg.jsx
@@ -8,11 +8,10 @@ const Blogg = ({ blog }) => {
             {/* <h1>Name:{blog.name}</h1> */}
             <div className="card bg-base-100 w-96 shadow-sm">
                 <figure>
-                    <img
-                        src={image}/>
+                    <img src={blog.image}/>
                 </figure>
                 <div className="card-body">
-                    <h2 className="card-title">Card Title</h2>
+                    <h2 className="card-title">Card Title: </h2>
                     <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Buy Now</button>
