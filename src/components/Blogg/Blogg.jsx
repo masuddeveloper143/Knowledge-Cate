cbea4e07a97e8e54445b4d936ceda54368e4d0eb
Masud Rana <md.masudrana.developer@gmail.com>
2026-04-08 01:41:59 +0800

    Update project

--- src/components/Blogg/Blogg.jsx
+++ src/components/Blogg/Blogg.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaBookmark } from "react-icons/fa6";
 
 const Blogg = ({ blog }) => {
     // const {blog} = props;
@@ -6,14 +7,26 @@ const Blogg = ({ blog }) => {
     return (
         <div>
             {/* <h1>Name:{blog.name}</h1> */}
-            <div className="card bg-base-100 w-96 shadow-sm">
+            <div className="card bg-base-100 w-96 shadow-smn my-6">
                 <figure>
-                    <img src={blog.image}/>
+                    <img src={blog.image} />
                 </figure>
                 <div className="card-body">
-                    <h1>crad name: {blog.name}</h1>
-                    <h2 className="card-title">Card Title: </h2>
-                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <h1>name: {blog.name}</h1>
+                    <h2 className="card-title">Card Title: {blog.title}</h2>
+                    <div className='flex justify-between items-center'>
+                        <h3 className='text-2xl'>{blog.author}</h3>
+                        <img className='rounded-full size-18 my-10' src={blog.author_image} />
+                        <FaBookmark  size={29}/>
+                    </div>
+
+                    <div className='flex'>
+                        {
+
+                            blog.hashtags.map((has) => <p>{has}</p>)
+
+                        }
+                    </div>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">mark as read</button>
                     </div>
